8b31a84f5b63179835321f7a1571f702cbabe936
rudaks <rudaks94@gmail.com>
2020-11-14 11:38:28 +0900

    update

--- chapter-10/src/main/java/jpabook/start/Team.java
+++ chapter-10/src/main/java/jpabook/start/Team.java
@@ -1,20 +1,14 @@
 package jpabook.start;
 
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.OneToMany;
-import javax.persistence.Temporal;
-import javax.persistence.TemporalType;
-
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 
 @Entity
 @Getter
